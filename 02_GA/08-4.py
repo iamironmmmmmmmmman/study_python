2ad92003d5e314bab2d3a273dea543a963880d58
Youkamii <gkfkd747@naver.com>
2024-10-10 21:56:10 +0900

    20241010

--- 02_GA/08-4.py
+++ 02_GA/08-4.py
@@ -4,4 +4,13 @@ string = """노트를 덮고
 저 금 Daydate rollie 넘을걸 {price}은 더
 """.format(man = "남자", price = "천백")
 
+string = """오늘 라면은 {s}를 {n}봉지 먹겠어
+""".format(s = "너구리", n = 7)
+
+# 오늘 점심으로 먹을 메뉴는 {이거}다!
+
+# 메뉴들 = [냉면, 제육, 라면, 솜사탕, 탕후루, 마라]
+
+# 메뉴들.3
+
 print(string)
