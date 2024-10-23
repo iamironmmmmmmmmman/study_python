046d7612ca0792097f77d168276cd14eecefbda0
Youkamii <gkfkd747@naver.com>
2024-10-23 17:57:45 +0900

    2024-10-23

---
+++ 04_GA/08_set.py
@@ -0,0 +1,17 @@
+myString = set("Hello")
+
+print (myString)
+
+# 중복은 사라진다.
+
+myString = list("Hello")
+
+print (myString)
+
+# 리스트화
+
+myString = tuple("Hello")
+
+print (myString)
+
+# 수정불가능한 리스트라고 생각하기 ( )
---
+++ 04_GA/09_set.py
@@ -0,0 +1,6 @@
+mySet1 = set([1,2,3,4,5,6])
+mySet2 = set([2,4,6,8,10])
+
+print(mySet1 & mySet2)
+
+# set 안에 []라는 리스트를 두고 중복처리
